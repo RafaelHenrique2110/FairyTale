c86c3cfae8200718e0d16976e8cc400cb620fc58
YrvingSouza <yrvingsc27@gmail.com>
2023-06-22 20:55:17 -0300

    menu de pausa

---
+++ FairyTale/Assets/Scrips/Canvas/PauseButton.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButton : MonoBehaviour
+{
+
+    private bool isPaused;
+
+    public GameObject menuPausa;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; // Pausa o tempo no jogo
+        isPaused = true;
+        menuPausa.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; // Retoma o tempo no jogo
+        isPaused = false;
+        menuPausa.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("Menu2");
+    }
+}
