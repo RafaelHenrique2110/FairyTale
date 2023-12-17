6b75a035b3620073dfe60916edf96022e871dfe6
Rafael Henrique <caverna2110@gmail.com>
2023-12-17 19:27:11 -0300

    cura

--- FairyTale/Assets/Scrips/Player/Poder/Poderes/Gerreiro/AddVida.cs
+++ FairyTale/Assets/Scrips/Player/Poder/Poderes/Gerreiro/AddVida.cs
@@ -6,11 +6,20 @@ public class AddVida: Poder_SO
 {
    [SerializeField] float vida;
     [SerializeField] Sprite indentificadorPoder;
-    
+    [SerializeField] float limiteTime;
+
     public override void ExecutarPoder()
     {
-        GameController.instance.Player.Curar(vida);
-       
 
+       GameObject vfxcura= Instantiate(GameController.instance.Protagonista.VfxCura(), GameController.instance.Protagonista.transform.position, GameController.instance.Protagonista.transform.rotation);
+        GameController.instance.Player.Curar(vida);
+       // GameController.instance.AtivarRemoverEfeitoPoder(limiteTime);
+        Destroy(vfxcura, limiteTime);
+
+
+    }
+    public override void RemoverEfeitoPoder()
+    {
+       // GameController.instance.Protagonista.VfxCura().SetActive(false);
     }
 }
